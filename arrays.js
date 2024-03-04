039bd343fc82d4eb1c4bac604f770a60ed818341
irene-nakabugo <irenenakabugo0@gmail.com>
2024-03-04 10:25:33 +0300

    pop method - arrays

--- arrays.js
+++ arrays.js
@@ -13,3 +13,13 @@ let names2=['Best', 'Raphy', 'Caleb', 'Cyrus']
 let combinedNames=names.concat(names2)
 
 console.log(combinedNames)
+
+//shift remove first
+//pop remove last element
+
+let pop=names.pop()
+console.log(pop)
+//unshift add first
+//push add last 
+
+//indexOf

fcff790ecbb330226d94b13f57ef4dce8150f3e7
irene-nakabugo <irenenakabugo0@gmail.com>
2024-03-04 09:53:11 +0300

    concat method - arrays
    add 2 or more arrays

--- arrays.js
+++ arrays.js
@@ -5,3 +5,11 @@ let names=['Irene', 'Miriam', 'Peruth', 'Chanel']
 let join =names.join('-')
 
 console.log(join)
+
+//concat method
+
+let names2=['Best', 'Raphy', 'Caleb', 'Cyrus']
+
+let combinedNames=names.concat(names2)
+
+console.log(combinedNames)

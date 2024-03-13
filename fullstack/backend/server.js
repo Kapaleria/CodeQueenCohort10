3a05b26869e9fefe8313169d15f202927ef83e59
irene-nakabugo <irenenakabugo0@gmail.com>
2024-03-13 10:19:08 +0300

    configure dotenv

--- fullstack/backend/server.js
+++ fullstack/backend/server.js
@@ -1,4 +1,7 @@
-const server = require ('express')
+require ('dotenv').config()
+const express = require ('express')
 const mongoose=require('mongoose')
 
 const app=express() //create an express app
+
+//const database=
